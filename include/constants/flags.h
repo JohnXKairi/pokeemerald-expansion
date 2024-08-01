3e7d3b6ff5671d79dcccd871f27fab41a10dcadb
j <john39clues@yahoo.gr>
2024-08-02 02:32:19 +0300

    starter fields end?

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1222,7 +1222,7 @@
 #define FLAG_UNUSED_0x494                                           0x494 // rocket102
 #define FLAG_UNUSED_0x495                                           0x495 // leaf burningfield
 #define FLAG_UNUSED_0x496                                           0x496 // rocket burning field
-#define FLAG_UNUSED_0x497                                           0x497 // Unused Flag
+#define FLAG_UNUSED_0x497                                           0x497 // leaf grassy
 #define FLAG_UNUSED_0x498                                           0x498 // Unused Flag
 #define FLAG_UNUSED_0x499                                           0x499 // Unused Flag
 #define FLAG_UNUSED_0x49A                                           0x49A // Unused Flag
